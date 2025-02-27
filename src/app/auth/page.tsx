c090d02b5179adfc0c6c1d5d75e42be7aa4f4ca0
bq22005 <bq22005@shibaura-it.ac.jp>
2025-02-27 17:16:48 +0900

    Change auth path

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import styles from "./page.module.css";
 import AuthForm from "@/app/components/layouts/AuthForm";
 
 export default function Auth() {
     return (
-        <div className={styles.signup}>
+        <div className={styles.auth}>
             <AuthForm />
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import styles from "./page.module.css";
-import AuthForm from "@/app/components/layouts/AuthForm";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <AuthForm />
+      <p>home</p>
     </div>
   );
 }
