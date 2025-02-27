153217d50609a66cf5eefefb382f486f6779edae
bq22005 <bq22005@shibaura-it.ac.jp>
2025-02-27 18:18:27 +0900

    Update header layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
+import styles from "./layout.module.css";
 
 const MPlusRounded1c = M_PLUS_Rounded_1c({
   weight: "400",
@@ -8,7 +9,7 @@ const MPlusRounded1c = M_PLUS_Rounded_1c({
 });
 
 export const metadata: Metadata = {
-  title: "Notifill",
+  title: "willtify",
   description: "未来の誰かにメッセージを届けよう。",
 };
 
@@ -20,6 +21,21 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${MPlusRounded1c.className}`}>
+        <header>
+          <div className={styles.container}>
+            <a href="">
+              <img className={styles.headerLogo} src="/next.svg" alt="" />
+            </a>
+            <nav className={styles.headerNav}>
+              <ul className={styles.navList}>
+                <li className={styles.navItem}><a href="/">Home</a></li>
+                <li className={styles.navItem}><a href="/">About</a></li>
+                <li className={styles.navItem}><a href="/">Letter box</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
         {children}
       </body>
     </html>
