35e7cd7a5c40bd98bbf9ef619922328959898c27
bq22005 <bq22005@shibaura-it.ac.jp>
2025-02-27 19:34:17 +0900

    Create groups of folder

---
+++ src/app/(Home)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.page}>
+            <h1>Home</h1>
+        </div>
+    );
+}
---
+++ src/app/(main)/letter/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Letter() {
+    return (
+        <div>
+            <h1>Letter</h1>
+        </div>
+    );
+}
