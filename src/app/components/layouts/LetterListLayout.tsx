dea91cf1f757f51fcdbbe50ad6e4139ef5bec49e
bq22005 <bq22005@shibaura-it.ac.jp>
2025-03-10 21:20:46 +0900

    Update the range of link

--- src/app/components/layouts/LetterListLayout.tsx
+++ src/app/components/layouts/LetterListLayout.tsx
@@ -4,13 +4,13 @@ export default function LetterListLayout() {
   return (
     <div className={styles.letterListLayout}>
       <div className={styles.container}>
-        <div className={styles.user}>
-          <img className={styles.userIcon} src="/user_default.png"/>
-          <a href="/">
+        <a href="">
+          <div className={styles.user}>
+            <img className={styles.userIcon} src="/user_default.png"/>
             <p className={styles.username}>username</p>
-          </a>
-          <p className={styles.date}>xxxx年x月xx日</p>
-        </div>
+            <p className={styles.date}>xxxx年x月xx日</p>
+          </div>
+        </a>
         <div className={styles.sentenceContainer}>
           <p className={styles.sentence} >sample</p>
         </div>
